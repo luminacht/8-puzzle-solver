35bebb9972904b1689427249dbbfacd82b78c847
luminacht <pmzxc11@gmail.com>
2021-11-17 15:49:29 +0800

    ids update

--- main.cpp
+++ main.cpp
@@ -1,62 +1,73 @@
-#include<iostream>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <ctime>
 using namespace std;
+
+// Graph class represents a directed graph using adjacency
+// list representation.
+class Graph
+{
+	int V; // No. of vertices
+
+	// Pointer to an array containing
+	// adjacency lists
+	list<int> *adj;
+
+	// A function used by IDDFS
+	bool DLS(int v, int target, int limit);
+
+public:
+	Graph(int V); // Constructor
+	void addEdge(int v, int w);
+
+	// IDDFS traversal of the vertices reachable from v
+	bool IDDFS(int v, int target, int max_depth);
+};
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w); // Add w to v’s list.
+}
+
+// A function to perform a Depth-Limited search
+// from given source 'src'
+bool Graph::DLS(int src, int target, int limit)
+{
+	if (src == target)
+		return true;
+
+	// If reached the maximum depth, stop recursing.
+	if (limit <= 0)
+		return false;
+
+	// Recur for all the vertices adjacent to source vertex
+	for (auto i = adj[src].begin(); i != adj[src].end(); ++i)
+	if (DLS(*i, target, limit-1) == true)
+		return true;
+
+	return false;
+}
+
+// IDDFS to search if target is reachable from v.
+// It uses recursive DFSUtil().
+bool Graph::IDDFS(int src, int target, int max_depth)
+{
+	// Repeatedly depth-limit search till the
+	// maximum depth.
+	for (int i = 0; i <= max_depth; i++)
+	if (DLS(src, target, i) == true)
+		return true;
+
+	return false;
+}
 
 int main(){
 
 }
 
-// Iterative deepning search.// 
-void IDS() {
-    int depth=0; // Depth cost.  
-
-    cout << "Starting IDS Algorithm... \n"; 
-
-    while(true){          
-        cur = start;     
-        fringe.push_front(cur);
-
-    while (!fringe.empty())     
-    {       // process the fringe states . 
-
-    cur = fringe.front();  
-
-    // if the front is goal.       
-    if (cur.is_goal()) {      
-
-        // calcilate the search time in msec.         
-        runtime = ((clock() * 1000) / CLOCKS_PER_SEC) - timer; 
-
-        // print the search costs.         
-        cout << "Time= " << runtime << "\n";         
-        cout << "Space= " << space << "\n";         
-        cout << "Cost= " << cur.g << "\n"; 
-
-        //print the solution path.         
-        cout << "Path:\n";         
-        PrintPath(&cur); 
-
-        // exit the function         
-        return;       
-        }
-
-        //if state not the goal and in the search depth.       
-        else if (depth > cur.g)      
-        {         
-            //expand the state.         
-            Expand();       
-        } 
-
-        else 
-        {   //not useable state.         
-            //pop it out         
-            fringe.pop_front();       
-        }     
-    }     
-    // clear both lists for the next round.     
-    fringe.clear();     
-    closed.clear();       
-    
-    // increase the search depth.     
-    depth++;   
-    } 
-}
