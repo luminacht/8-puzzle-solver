7da6c00d796a16cf5f6cc4c6440c836f856917f9
luminacht <pmzxc11@gmail.com>
2021-11-22 16:49:04 +0800

    poggers

--- main.cpp
+++ main.cpp
@@ -2,19 +2,30 @@
 // for N*N -1 puzzle algorithm using Branch and Bound
 // The solution assumes that instance of puzzle is solvable
 #include <bits/stdc++.h>
+#include <list>
 using namespace std;
 #define N 3
-#define BLANK_CHARACTER '0'
+#define BLANK '0'
 
-typedef enum Move {
+typedef enum Moves {
     UP, DOWN, LEFT, RIGHT, //values for moving up, down, left, right, respectively
-    NOT_APPLICABLE         //value assigned for initial and goal input states
-} Move;
+    INPUT         //value assigned for initial and goal input states
+} Moves;
 
 typedef struct State {
-    Move action;           //action that resulted to this board state
+    Moves action;           //action that resulted to this board state
     char mat[N][N];      //resulting board configuration after applying action
+    Node *parent;
 } State;
+
+class Node;
+
+list<Node> closed, fringe;
+
+typedef struct Solution {
+    Moves action;
+    struct Solution *next;
+} Solution;
 
 void inputState(State * const state) {
 
@@ -24,7 +35,7 @@ void inputState(State * const state) {
     /*Counter variables for the loop*/
     char i, j;
     char tempCheck[9] = {0};
-    state->action = NOT_APPLICABLE;
+    state->action = INPUT;
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
@@ -74,10 +85,92 @@ int calculateCost(int initial[N][N], int final[N][N])
     return count;
 }
 
-// Function to check if (x, y) is a valid matrix coordinate
-int isSafe(int x, int y)
+// State Expanding Function.
+void Expand()
 {
-    return (x >= 0 && x < N && y >= 0 && y < N);
+    State cur, temp;
+
+    //add current state to the closed list.
+    closed.push_back(cur);
+    int i, j;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            //finding the 0 element in the state array.
+            if (cur.mat[i][j] == 0)
+            {
+                // if the 0 not in the first row.
+                if (i > 0)
+                {
+                    // set the  basic elements
+                    temp = cur;
+                    temp.parent = &(closed.back());
+                    // shift the zero element UP..
+                    swap(temp.mat[i][j], temp.mat[i - 1][j]);
+                    // search for the child in the closed list.
+                    // if the child not found in the closed list.
+                    if (!InClosed(temp))
+                    {
+                        //set remaining elements
+                        temp.g += 1;
+                        temp.Heurs();
+                        temp.t = temp.g + temp.h;
+                        fringe.push_front(temp); //push the child into the fringe list
+                                                 //space++;//increment the space counter
+                    }
+                }
+                //if the 0 is not in the last row.
+                if (i < N - 1)
+                {
+                    temp = cur;
+                    temp.parent = &(closed.back());
+                    //shift the zero element DOWN.
+                    swap(temp.mat[i][j], temp.mat[i + 1][j]);
+                    if (!InClosed(temp))
+                    {
+                        temp.g += 1;
+                        temp.Heurs();
+                        temp.t = temp.g + temp.h;
+                        fringe.push_front(temp);
+                        //space++;
+                    }
+                } // if the 0 element not in the first column.
+                if (j > 0)
+                {
+                    temp = cur;
+                    temp.parent = &(closed.back());
+                    // shift it LEFT.
+                    swap(temp.mat[i][j], temp.mat[i][j - 1]);
+                    if (!InClosed(temp))
+                    {
+                        temp.g += 1;
+                        temp.Heurs();
+                        temp.t = temp.g + temp.h;
+                        fringe.push_front(temp);
+                        //space++;
+                    }
+                } // if the zero elemnt not in the last column.
+                if (j < N - 1)
+                {
+                    temp = cur;
+                    temp.parent = &(closed.back());
+                    //shift is right
+                    swap(temp.mat[i][j], temp.mat[i][j + 1]);
+                    if (!InClosed(temp))
+                    {
+                        temp.g += 1;
+                        temp.Heurs();
+                        temp.t = temp.g + temp.h;
+                        fringe.push_front(temp);
+                        //space++;
+                    }
+                }
+            }
+        }
+    }
+    //remove the expanded state from the fringe list
+    fringe.remove(cur);
 }
 
 // Driver code
