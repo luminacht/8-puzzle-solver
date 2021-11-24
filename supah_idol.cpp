762d25d400b4d6fdfdf3732802415041af43649e
raffittee <63999782+raffittee@users.noreply.github.com>
2021-11-24 22:51:18 +0800

    Supah_Idol v.2
    Added functions:
    heuristics
    checking goal state
    inc astar search

--- supah_idol.cpp
+++ supah_idol.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <bits/stdc++.h>
 #include <queue>
+#include <list>
 #include <algorithm>
 #include <string>
 #include <time.h>
@@ -11,12 +11,9 @@ using namespace std;
 class State
 {
 public:
-    State *parent; // parent node
-    State();
     char state[N][N]; // state array of the board
     int gc, hc;       // gc = cost, hc = heuristic cost
-
-    State printMatrix()
+    void printMatrix()
     {
         printf("\n");
         for (int i = 0; i < N; i++)
@@ -31,7 +28,7 @@ public:
         printf("\n");
     }
 
-    int manhattanDistance()
+    int heuristics()
     {
         int sum = 0;
         for (int i = 0; i < N; i++)
@@ -48,6 +45,98 @@ public:
         }
         return sum;
     }
+
+    int inputState()
+    {
+
+        // Initial configuration
+        // Value 0 is used for empty space
+        /* 2D array declaration*/
+        /*Counter variables for the loop*/
+        char i, j;
+        char holdCheck[9] = {0};
+
+        for (i = 0; i < N; i++)
+        {
+            for (j = 0; j < N; j++)
+            {
+                int holder;
+                printf("matrix[%d][%d]:", i, j);
+                scanf("%d", &holder);
+
+                if (holder >= 0 && holder < 9)
+                    if (!holdCheck[holder])
+                    {
+                        state[i][j] = holder;
+                        holdCheck[holder] = 1;
+                    }
+                    else
+                    {
+                        printf("   ERROR: Number %d is already used. Try again with different input.\n", holder);
+                        --j;
+                    }
+                else
+                {
+                    printf("   ERROR: Invalid input. Enter a number from 0 to 8.\n");
+                    --j;
+                }
+            }
+        }
+    }
+
+    int isGoalState()
+    {
+        int i, j;
+        for (i = 0; i < N; i++)
+        {
+            for (j = 0; j < N; j++)
+            {
+                if (state[i][j] != (i * N + j))
+                    return 0;
+            }
+        }
+        return 1;
+    }
+    /*int heuristics()
+{
+    int i, i2, j, j2, 
+    hc = 0;
+    bool found;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            found = false;
+            for (i2 = 0; i2 < N; i2++)
+            {
+                for (j2 = 0; j2 < N; j2++)
+                {
+                    hc += abs(i - i2) + abs(j - j2);
+                    printf("Total Heuristic Cost: %d", &hc);
+                    // finding similar elements.
+                    if (state[i][j] == state[i2][j2])
+                    {
+                        //Manhattan Based Heuristic displacement cost.
+                        found = true;
+                        return hc;
+                    }
+                }
+            }
+        }
+    } 
+}*/
+};
+
+class Searches // class for all search algorithms
+{
+public:
+    char State::state[N][N]; // state array of the board
+    list<State> open, closed;
+
+    void aStar(State state s, State state g)
+    {
+        open.push_back()
+    }
 };
 
 int main()
@@ -56,21 +145,10 @@ int main()
     State goalState;
 
     cout << "Enter the initial state: " << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            cin >> initialState.state[i][j];
-        }
-    }
+    initialState.inputState();
     initialState.printMatrix();
 
     cout << "Enter the goal state: " << endl;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            cin >> goalState.state[i][j];
-        }
-    }
+    goalState.inputState();
+    goalState.printMatrix();
 }
